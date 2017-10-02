a5286c624d8c512a4f0daf302facad223f440a6a
BrookeMa <maye634028187@icloud.com>
2017-10-02 17:25:35 +0800

    完善realm 模型

--- component/Realm/realm.js
+++ component/Realm/realm.js
@@ -11,10 +11,10 @@ Question.schema = {
     properties: {
 
         uid: 'string',                                           /// 标识 ID        
-        analysis: { type: 'string', optional: true },            /// 分析
+        analysis: { type: 'string', optional: true },            /// 解析
         answer: 'string',                                        /// 答案
         category_of_problem: 'string',                           /// 题目类型: "资料分析"  "常识判断" "言语理解与表达"  "数量关系" "判断推理"
-        expand: { type: 'string', optional: true },              /// 扩展
+        expand: { type: 'string', optional: true },              /// 拓展
         number: 'int',                                           /// 题号
         option_A: { type: 'string', optional: true },            /// 选项 A
         option_B: { type: 'string', optional: true },            /// 选项 B
@@ -22,11 +22,12 @@ Question.schema = {
         option_D: { type: 'string', optional: true },            /// 选项 D
         paraphrase: { type: 'string', optional: true },          /// 释义
         province: { type: 'string' },                            /// 省份
-        recipe: { type: 'string', optional: true },              /// 秘技
+        recipe: { type: 'string', optional: true },              /// 技巧
         type_of_problem: { type: 'string', optional: true },     /// 类型 '单选题'
         type: 'string',
         name_of_paper: 'string',
         error_correction: { type: 'string', optional: true },    /// 纠错
+        examination_point: { type: 'string', optional: true },   /// 考点
     }
 }
 
@@ -39,15 +40,15 @@ User.schema = {
     properties: {
 
         id: 'string',
-        account: { type: 'string', optional: true },            /// 账号
-        avatarURL: { type: 'string' },                          /// 头像
+        account: { type: 'string', optional: true },              /// 账号
+        avatarURL: { type: 'string' },                            /// 头像
         token: { type: 'string', },
-        examinationPapers: {type: 'list', objectType: 'string'} /// 购买的试卷
+        examinationPapers: { type: 'list', objectType: 'string' } /// 购买的试卷id
     }
 }
 
 /// 试卷
-class ExaminationPaper extends Realm.Object { } 
+class ExaminationPaper extends Realm.Object { }
 
 ExaminationPaper.schema = {
 
@@ -55,12 +56,46 @@ ExaminationPaper.schema = {
     properties: {
 
         id: 'string',
-        year: 'string',
-        province: 'string',
-        version: 'string',
-        price: { type: 'float', optional: true },
+        year: 'string',                                         /// 年份
+        province: 'string',                                     /// 所在省份
+        version: 'string',                                      /// 当前版本
+        purchased: 'bool',                                      /// 是否已购
+        price: { type: 'float', optional: true },               /// 价格，允许免费
         questions: { type: 'list', objectType: 'Question' },
     }
 }
 
-export default new Realm({ schema: [Question, User, ExaminationPaper] });
+/// 记忆模型
+class MemoryModel extends Realm.Object { }
+
+MemoryModel.schema = {
+
+    name: 'MemoryModel',
+    properties: {
+
+        question: 'Question',                                   /// 题目
+        weighting: { type: 'float', default: 0 },               /// 加权分数, 初始化为 0
+        appearedSeveralTime: { type: 'int', default: 0 },     /// 出现次数, 初始化为 0
+        lastAppearTime: { type: 'date', default: 0 },         /// 上一次出现时间，初始化为 0
+
+    }
+}
+
+/// 进度规划
+class Schedule extends Reaml.Object { }
+
+Schedule.schema = {
+
+    name: 'Schedule',
+    properties: {
+
+        id: 'string',
+        date: { type: 'date' },
+        learnTotal:  {type: 'int', default: 0},
+        needLearnTotal: {type: 'int', default: 0},
+        memoryModels: { type: 'list', objectType: 'MemoryModel' },
+        totalQuestions: { type: 'list', objectType: 'Question' }      /// 总提数
+    }
+}
+
+export default new Realm({ schema: [Question, User, ExaminationPaper, MemoryModel, Schedule] });
--- component/Realm/realmManager.js
+++ component/Realm/realmManager.js
@@ -5,19 +5,92 @@ import realm from './realm';
 export default class RealmManager {
 
 
-    static setItem() {
-
-        
-    }
-
+    /// 创建
     static createQuestion(question) {
+
         try {
             realm.write(() => {
                  realm.create('Question', question);
             });
         } catch (e) {
-             console.log("Error on creation");
+             console.log("Question Error on creation");
         }
+    }
+
+    static createExaminationPaper(examinationPaper) {
+
+        try {
+            realm.write(() => {
+                 realm.create('ExaminationPaper', examinationPaper);
+            });
+        } catch (e) {
+             console.log("ExaminationPaper Error on creation");
+        }
+    }
+
+    static createUser(user) {
+
+        try {
+            realm.write(() => {
+                 realm.create('User', user);
+            });
+        } catch (e) {
+             console.log("User Error on creation");
+        }
+    }
+
+    static createMemoryModel(memoryModel) {
+
+        try {
+            realm.write(() => {
+                 realm.create('MemoryModel', memoryModel);
+            });
+        } catch (e) {
+             console.log("MemoryModel Error on creation");
+        }
+    }
+
+    static createSchedule(schedule) {
+
+        try {
+            realm.write(() => {
+                 realm.create('Schedule', schedule);
+            });
+        } catch (e) {
+             console.log("Schedule Error on creation");
+        }
+    }
+
+    
+    /// 更新
+    static updateQuestion(question) {
+
+
+    }
+
+
+    /// 查询数据
+    // 获取当前用户
+    static getCurrentUser() {
+
+        let users = realm.objects('User');
+
+        if (users.length == 0) {
+
+            return null;
+
+        } else {
+
+            return users[0];
+        }
+    }
+
+    // 通过id获取指定试卷
+    static getExaminationPaper(id) {
+
+        let examinationPaper = realm.objects('ExaminationPaper').filtered('id=' + id + '')[0];
+
+        return examinationPaper;
     }
 }
 
