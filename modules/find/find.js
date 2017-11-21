8f6b230b8ac2f2a3d1396aabdcb5be37f39dec5c
mike <573940421@qq.com>
2017-11-21 23:43:28 +0800

    find layout 变更

--- modules/find/find.js
+++ modules/find/find.js
@@ -14,13 +14,9 @@ import { Calendar, LocaleConfig } from 'react-native-calendars';
 import Echarts from 'native-echarts';
 import { newPaper ,pieOption} from './chartOptions';
 
-LocaleConfig.locales['cn'] = {
-    monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-    monthNamesShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-    dayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
-    dayNamesShort: ['日', '一', '二', '三', '四', '五', '六']
-};
-LocaleConfig.defaultLocale = 'cn';
+
+
+const clientWidth = 375;
 
 export default class Find extends Component {
 
@@ -41,16 +37,28 @@ export default class Find extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.title}>
-                    <View style={styles.text}>
-                        <Text style={styles.h2}>2017年北京省考</Text>
-                        <Text style={styles.p}>历年真题</Text>
+                    <View style={styles.titleContent}>
+                        <View style={styles.text}>
+                            <Text style={styles.h2}>2017年北京省考</Text>
+                            <Text style={styles.p}>历年真题</Text>
+                        </View>
+                        <View style={styles.circleChart}> 
+                            <Echarts option={pieOption.option} height={90} />
+                        </View>
                     </View>
-                    <View style={styles.circleChart}>
-						<Echarts option={pieOption.option} height={80} />
-					</View>
                 </View>
-                <View style={styles.chartsView}>
-                    <Echarts option={newPaperOption} height={220} />
+                <View style={styles.calendarView}>
+                    <View style={styles.chartTitle}>
+                        <View style={styles.chartTitleLeft}>
+                            <Text style={styles.h4}>过去6日刷题亮统计</Text>
+                            <Text style={styles.psmall}>历年真题</Text>
+                        </View>
+                        <View style={styles.chartTitleRight}>
+                            <Text style={styles.h4}>本月共进行6次</Text>
+                            <Text style={styles.psmall}>最后刷题日：今日</Text>
+                        </View>
+                    </View>
+                    <Echarts style={styles.chart} option={newPaperOption} height={clientWidth*0.6} />
                 </View>
                 <View style={styles.chartsView}>
                     <Echarts option={newPaperOption} height={250} />
@@ -64,24 +72,8 @@ export default class Find extends Component {
         });
     }
 }
-const calenderOptiones = {
-    Theme: {
-        calendarBackground: '#ffffff',
-        textSectionTitleColor: '#b6c1cd',
-        selectedDayBackgroundColor: '#FFA200',
-        selectedDayTextColor: '#ffffff',
-        todayTextColor: '#00adf5',
-        dayTextColor: '#7C86A2',
-        textDisabledColor: '#E1E4E7',
-        dotColor: '#00adf5',
-        selectedDotColor: '#ffffff',
-        arrowColor: '#2d4150',
-        monthTextColor: '#2d4150'
-    },
-    selected: '2017-06-17'
-};
 
-const styles = StyleSheet.create({
+const styles = {
     container: {
         flex: 1,
         width: '100%',
@@ -96,18 +88,72 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     title: {
-        flex: 2,
+        paddingBottom:5,
+        backgroundColor:"#F1F4FB",
+        height:clientWidth*0.253,
+    },
+    titleContent:{
         flexDirection:"row",
+        // paddingBottom:10,
+        // paddingRight:20,
     },
     text:{
-        flex: 7,
+        flex: 6,
+        paddingTop:20,
+        paddingLeft:15,
+        backgroundColor:"#fff"
+    },
+    h2:{
+        fontSize:24,
+        lineHeight:30
+    },
+    p:{
+        marginTop:5,
+        fontSize:14,
+        lineHeight:20,
+        color:"#8E9091"
     },
     circleChart:{
-        flex: 3,
+        flex: 4,
     },
     calendarView: {
-        flex: 5,
-        // height: 300,
+        // flex: 5,
+        paddingTop:20,
+        backgroundColor:'green',
+        height: clientWidth*0.8,
+        position:'relative'
+    },
+    chartTitle:{
+        flexDirection:"row",
+        position:"absolute",
+        width:'100%',
+        height:55,   
+        left:0,
+        top:0,
+        zIndex:100     
+    },
+    chartTitleLeft:{
+        flex: 11,
+        paddingTop:20,
+        paddingLeft:15,
+        backgroundColor:"#fff"
+    },
+    chartTitleRight:{
+        flex: 9,
+        paddingTop:20,
+        backgroundColor:"#fff"
+    },
+    chart:{
+        // marginTop:200,
+    },
+    h4:{
+        fontSize:14,
+        lineHeight:16,
+    },
+    psmall:{
+        fontSize:14,
+        lineHeight:16,
+        color:"#8E9091"
     },
     calender: {
         paddingTop: 5,
@@ -115,9 +161,10 @@ const styles = StyleSheet.create({
         borderColor: '#eee',
     },
     chartsView: {
-        flex: 5,
+        height:300,
+        backgroundColor:'black'        
     }
-});
+}
 
 //                <View style={styles.calendarView} >
 //                     <Calendar
@@ -131,3 +178,28 @@ const styles = StyleSheet.create({
 //                         onDayPress={this.onDayPress}
 //                     />
 //                 </View>
+
+// const calenderOptiones = {
+//     Theme: {
+//         calendarBackground: '#ffffff',
+//         textSectionTitleColor: '#b6c1cd',
+//         selectedDayBackgroundColor: '#FFA200',
+//         selectedDayTextColor: '#ffffff',
+//         todayTextColor: '#00adf5',
+//         dayTextColor: '#7C86A2',
+//         textDisabledColor: '#E1E4E7',
+//         dotColor: '#00adf5',
+//         selectedDotColor: '#ffffff',
+//         arrowColor: '#2d4150',
+//         monthTextColor: '#2d4150'
+//     },
+//     selected: '2017-06-17'
+// };
+
+// LocaleConfig.locales['cn'] = {
+//     monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+//     monthNamesShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+//     dayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+//     dayNamesShort: ['日', '一', '二', '三', '四', '五', '六']
+// };
+// LocaleConfig.defaultLocale = 'cn';
