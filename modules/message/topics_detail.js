fbd20fe01dc88be8de9c9c7e29ed5f6d1430d02d
BrookeMa <maye634028187@icloud.com>
2017-11-24 22:08:38 +0800

    题库展示相关

--- modules/message/topics_detail.js
+++ modules/message/topics_detail.js
@@ -4,16 +4,18 @@ import {
     Text,
     View,
     TouchableOpacity,
-    ScrollView,
-    FlatList,
-    Vibration,
-    StyleSheet
+    FlatList
 } from 'react-native';
 
 export default class TopicsDeail extends React.Component {
 
     static navigationOptions = ({ navigation, screenProps }) => ({
-		title: navigation.state.params.section.item.title,
+        title: navigation.state.params.section.item.title,
+        headerTitleStyle: {
+            color: 'black', 
+            alignSelf: 'center',
+            fontSize: 20 
+        },
 		headerStyle: {
 			backgroundColor: '#FFF',
             opacity: 1,
@@ -35,13 +37,38 @@ export default class TopicsDeail extends React.Component {
     
     constructor(props) {
         super(props)
+        console.log(this.props.navigation.state.params.section.item.data)
+        this.state = {
+            data:this.props.navigation.state.params.section.item.data
+        }
 
+    }
 
+    _renderHeader() {
+        return (
+            <View style={styles.listHeader}>
+                <Text style={styles.listTitle}>{this.state.data.length}套真题</Text>
+                <View style={styles.bottomLine}></View>
+            </View>
+        )
+    }
+
+    _renderItem(item) {
+        return (
+            <View style={styles.itemView}>
+                <Text style={styles.itemText}>{item.value}</Text>
+            </View>
+        )
     }
 
     render() {
         return (
-            <View></View>
+            <FlatList
+            ListHeaderComponent={this._renderHeader()}
+            data={this.state.data}
+            keyExtractor={(item, index) => index}            
+            renderItem={({item}) => this._renderItem(item)}
+          />
         )
     }
 }
@@ -53,5 +80,34 @@ var styles = ({
         alignItems: 'center',
         width: 44,
         height: 44,
+    },
+    listHeader: {
+        marginTop: 4,
+        height: 34, 
+        backgroundColor: 'white'
+    },  
+    listTitle: {
+        marginTop: 11,
+        fontSize: 12,
+        color: "#9B9B9B",
+        marginLeft: 15,
+    },
+    bottomLine: {
+        position:"absolute",
+        right: 15,
+        left: 15,
+        bottom: 1,
+        backgroundColor: "#979797",
+        height: 0.3,
+    },
+    itemView: {
+        backgroundColor: "white",
+        height: 41,
+    },
+    itemText: {
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 60,
+        fontSize: 13,
     }
 })
