67a324c50ad1fe4a99c109bcb67ab99137905f06
mike <573940421@qq.com>
2018-01-28 17:44:55 +0800

    单图情况处理

--- component/detail/option.js
+++ component/detail/option.js
@@ -7,7 +7,7 @@ import {
     Image,
     TouchableOpacity,
 } from 'react-native';
-import {webURL, imageWebURL} from "../../service/constant"
+import { webURL, imageWebURL } from "../../service/constant"
 const Dimensions = require('Dimensions');
 const window = Dimensions.get('window');
 
@@ -30,13 +30,7 @@ export default class Option extends React.Component {
         that.props.select(option);
     }
 
-    _handleImageURL(content) {
 
-        var re2 = /\/.*?\.(?:png|jpg)/gm;
-        let suffixUrl = re2.exec(content)
-
-        return imageWebURL + suffixUrl
-    }
 
     _afterSelectText() {
 
@@ -123,6 +117,91 @@ export default class Option extends React.Component {
         let regex = /<img/g
         let splits = str.split(regex)
 
+        const _handleImageURL = (content) => {
+
+            var re2 = /\/.*?\.(?:png|jpg)/gm;
+            let suffixUrl = re2.exec(content)
+
+            return imageWebURL + suffixUrl
+        }
+        //获取属性中的wdith 和 height
+        getAttr = (str) => {
+            let widthReg = /width=[\'\"]?([^\'\"]*)[\'\"]?/i,
+                heightReg = /height=[\'\"]?([^\'\"]*)[\'\"]?/i;
+            let widthStr = str.match(widthReg),
+                heightStr = str.match(heightReg);
+            let obj = {}
+            if(!!widthStr){
+                obj['width'] = widthStr[1]
+            }
+            if(!!heightStr){
+                obj['height'] = heightStr[1]
+            }
+            return obj
+
+        }
+        //获取style 属性
+        const getStyle = (str) => {
+            let styleReg = /style=[\'\"]?([^\'\"]*)[\'\"]?/i;
+            let styleStr = str.match(styleReg);
+            let obj = {}
+            if (!!styleStr) {
+                //可能在style里
+                let styleArr = styleStr[1].split(';')
+                styleArr.forEach(res => {
+                    if (([], res.split(':'))[0].indexOf('width') >= 0) {
+                        obj['width'] = ([], res.split(':'))[1]
+                    }
+                    if (([], res.split(':'))[0].indexOf('height') >= 0) {
+                        obj['height'] = ([], res.split(':'))[1]
+                    }
+                })
+            }
+            return obj
+        }
+        const renderContent = () => {
+            //单图情况
+            if (splits.length === 2 && !splits[0]) {
+                let url = _handleImageURL(splits[1])
+                let styleObj = getStyle(str)
+                let attrObj = getAttr(str)
+                
+                let width, height;
+
+                let expr = /\/(.*)_(.*)x(.*)_/;
+                let size = url.match(expr)
+                const scale = 0.3
+                width = attrObj.width || styleObj.width || size[2] * scale /// cannot read 2 TODO
+                height = attrObj.height || styleObj.height || size[3] * scale
+                return (<Image key={index} style={[styles.optionImage, { width: width, height: height }]} resizeMode={'contain'} source={{ uri: url }} />)
+            }
+            //文字与图嵌套
+            return splits.map((content, index) => {
+                if (content.search(/.\/(.*)png/g) >= 0 || content.search(/.\/(.*)jpg/g) >= 0) {
+
+
+                    let url = _handleImageURL(content)
+                    let expr = /\/(.*)_(.*)x(.*)_/;
+                    let size = url.match(expr)
+                    const scale = 0.3
+                    let width = size[2] * scale /// cannot read 2 TODO
+                    let height = size[3] * scale
+
+                    if (size[1].search("formula")) {
+                        width = size[2] * (23 / size[3])
+                        height = 23
+                    }
+
+                    return (
+                        <Image key={index} style={[styles.optionImage, { width: width, height: height }]} resizeMode={'contain'} source={{ uri: url }} />
+                    )
+                } else {
+                    return (
+                        <Text key={index} style={[styles.detailOptionText, this._afterSelectText()]}>{content}</Text>
+                    )
+                }
+            })
+        }
         return (
             <View style={[styles.answerItem, this._afterSelectBackgroundView()]} >
                 <Image
@@ -131,31 +210,7 @@ export default class Option extends React.Component {
                     source={this._selectIcon()}
                 />
                 <View style={styles.detailOptionView}>
-                    {
-                        splits.map((content, index) => {
-                            if (content.search(/.\/(.*)png/g) >= 0 || content.search(/.\/(.*)jpg/g) >= 0) {
-                                let url = this._handleImageURL(content)
-                                let expr = /\/(.*)_(.*)x(.*)_/;
-                                let size = url.match(expr)
-                                const scale = 0.3
-                                let width = size[2] * scale /// cannot read 2 TODO
-                                let height = size[3] * scale
-
-                                if (size[1].search("formula")) {
-                                    width = size[2] * (23 / size[3])
-                                    height = 23
-                                }
-
-                                return (
-                                    <Image key={index} style={[styles.optionImage, { width: width, height: height }]} resizeMode={'contain'} source={{ uri: url }} />
-                                )
-                            } else {
-                                return (
-                                    <Text key={index} style={[styles.detailOptionText, this._afterSelectText()]}>{content}</Text>
-                                )
-                            }
-                        })
-                    }
+                    {renderContent()}
                 </View>
             </View>
         );
