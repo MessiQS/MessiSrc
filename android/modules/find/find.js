d1e708bc19e7533f343885b57a76d4ff635f25f1
MK <>
2018-07-28 14:14:15 +0800

    更新UI

--- android/modules/find/find.js
+++ android/modules/find/find.js
@@ -267,21 +267,20 @@ export default class Find extends Component {
         }
 
         return (
-            <TouchableOpacity onPress={this.routeToClassification.bind(this)} >
-                <View style={styles.titleContent}>
-                    <Image style={styles.greenBlock} source={require("../../../Images/green_block.png")} />
-                    <View style={styles.titleText}>
-                        <Text numberOfLines={1} style={[styles.h2, styles.examTitle]}>{this.state.currentExam}</Text>
-                        <Text style={[styles.p, styles.examDetail]}>{this.state.currentExamDetail}</Text>
-                    </View>
-                    <Animated.View style={[styles.circleChart, {
-                        opacity: this.state.fadeInOpacity
-                    }]}>
-                        <Echarts option={option} width={60} height={60} />
-                    </Animated.View>
-                    <Image style={[styles.arrow, { top: 23 }]} source={require("../../../Images/find_arrow_right.png")} />
-                </View>
-            </TouchableOpacity>
+            <View style={styles.titleContent}>
+				<Image style={styles.greenBlock} source={require("../../../Images/green_block.png")} />
+				<View style={styles.titleText}>
+					<Text numberOfLines={2} style={[styles.h2, styles.examTitle]}>{this.state.currentExam}</Text>
+				</View>
+				<Animated.View style={[styles.circleChart, {
+					opacity: this.state.fadeInOpacity
+				}]}>
+					<Echarts option={option} width={60} height={60} />
+				</Animated.View>
+				<TouchableOpacity style={styles.choosePaperButton} onPress={this.routeToClassification.bind(this)} >
+					<Text style={[styles.button, { borderColor: '#1AAD19', borderWidth: 1, color: "#1AAD19"}]}>选题库</Text>
+				</TouchableOpacity>
+			</View>
         )
     }
 
@@ -343,28 +342,27 @@ export default class Find extends Component {
 
         return (
             <View style={styles.calendarView}>
-                <View style={styles.chartTitleContainer}>
-                    <TouchableOpacity onPress={this.routeToNewDetail.bind(this)}>
-                        <View style={styles.chartTopContainer}>
-                            <Image style={styles.redBlock} source={require("../../../Images/blue_block.png")} />
-                            <Text style={styles.h4}>过去5日刷题数量统计</Text>
-                            <Text style={[styles.rightTitle, { color: "#1495EB" }]}>刷新题</Text>
-                            <Image style={[styles.arrow, { top: 2 }]} source={require("../../../Images/find_arrow_right.png")} />
-                        </View>
-                    </TouchableOpacity>
-                    <View style={styles.separator} />
-                    <View style={styles.chartBottomContainer}>
-                        <Text style={styles.psmall}>最后刷题日：{this.state.info.newLastSelectDate}</Text>
-                        <Text style={styles.average}>平均値：{this.state.info.newAverage}</Text>
-                        <Text style={styles.rightDetail}>剩余：{this.state.info.newQuestionCount}</Text>
-                    </View>
-                </View>
-                <Animated.View style={{
-                    opacity: this.state.fadeInOpacity
-                }}>
-                    <Echarts option={newPaperOption} height={width * 0.6} />
-                </Animated.View>
-            </View>
+				<View style={styles.chartTitleContainer}>
+					<View style={styles.chartTopContainer}>
+						<Image style={styles.redBlock} source={require("../../../Images/blue_block.png")} />
+						<Text style={styles.h4}>过去5日刷题数量统计</Text>
+						<TouchableOpacity style={[styles.rightTitle]} onPress={this.routeToNewDetail.bind(this)}>
+							<Text style={[styles.button, { borderColor: '#1495EB', borderWidth: 1, color: "#1495EB"}]}>刷新题</Text>
+						</TouchableOpacity>
+					</View>
+					<View style={styles.separator} />
+					<View style={styles.chartBottomContainer}>
+						<Text style={styles.psmall}>最后刷题日：{this.state.info.newLastSelectDate}</Text>
+						<Text style={styles.average}>平均値：{this.state.info.newAverage}</Text>
+						<Text style={styles.rightDetail}>剩余：{this.state.info.newQuestionCount}</Text>
+					</View>
+				</View>
+				<Animated.View style={{
+					opacity: this.state.fadeInOpacity
+				}}>
+					<Echarts option={newPaperOption} height={width * 0.6} />
+				</Animated.View>
+			</View>
         )
     }
 
@@ -421,30 +419,30 @@ export default class Find extends Component {
         // }
 
         return (
-            <View style={styles.calendarView}>
-                <View style={styles.chartTitleContainer}>
-                    <TouchableOpacity onPress={this.routeToWrongDetail.bind(this)}>
-                        <View style={styles.chartTopContainer}>
-                            <Image style={styles.redBlock} source={require("../../../Images/red_block.png")} />
-                            <Text style={styles.h4}>未来5日遗忘数量预测</Text>
-                            <Text style={[styles.rightTitle, { color: "#FF5B29" }]}>刷错题</Text>
-                            <Image style={[styles.arrow, { top: 2 }]} source={require("../../../Images/find_arrow_right.png")} />
-                        </View>
-                    </TouchableOpacity>
-                    <View style={styles.separator} />
-                    <View style={styles.chartBottomContainer}>
-                        <Text style={styles.psmall}>最后刷题日：{this.state.info.wrongLastSelectDate}</Text>
-                        <Text style={styles.average}>平均値：{this.state.info.wrongAverage}</Text>
-                        <Text style={styles.rightDetail}>剩余：{this.state.info.wrongQuestionCount}</Text>
-                    </View>
-                </View>
-                <Animated.View style={{
-                    opacity: this.state.fadeInOpacity
-                }}>
-                    <Echarts option={newPaperOption} height={width * 0.6} />
-                </Animated.View>
-            </View>
-        )
+			<View style={styles.calendarView}>
+				<View style={styles.chartTitleContainer}>
+						<View style={styles.chartTopContainer}>
+							<Image style={styles.redBlock} source={require("../../../Images/red_block.png")} />
+							<Text style={styles.h4}>未来5日遗忘数量预测</Text>
+							<TouchableOpacity style={styles.rightTitle} onPress={this.routeToWrongDetail.bind(this)}>
+								<Text style={[styles.button, { borderColor: '#FF5B29',
+		borderWidth: 1, color: "#FF5B29"}]}>刷错题</Text>
+							</TouchableOpacity>
+						</View>
+					<View style={styles.separator} />
+					<View style={styles.chartBottomContainer}>
+						<Text style={styles.psmall}>最后刷题日：{this.state.info.wrongLastSelectDate}</Text>
+						<Text style={styles.average}>平均値：{this.state.info.wrongAverage}</Text>
+						<Text style={styles.rightDetail}>剩余：{this.state.info.wrongQuestionCount}</Text>
+					</View>
+				</View>
+				<Animated.View style={{
+					opacity: this.state.fadeInOpacity
+				}}>
+					<Echarts option={newPaperOption} height={width * 0.6} />
+				</Animated.View>
+			</View>
+		)
     }
 
     render() {
@@ -461,153 +459,161 @@ export default class Find extends Component {
         )
     }
 }
-
 const styles = {
-    container: {
-        backgroundColor: '#F6F6F6',
-        width: '100%',
-        height: '100%'
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    titleContent: {
-        marginTop: Math.min(Math.max((height - 64 - 78 - (2 * 304)) / 3, 3), 10),
-        flexDirection: "row",
-        backgroundColor: "white",
-        height: 78,
-    },
-    arrow: {
-        position: 'absolute',
-        resizeMode: 'contain',
-        right: 18.8,
-        width: 7.4,
-    },
-    titleText: {
-        width: '70%',
-        height: '100%',
-        backgroundColor: "#fff",
-        marginLeft: 10,
-    },
-    h2: {
-        fontSize: 16,
-        lineHeight: 25,
-        color: "#172434",
-    },
-    examTitle: {
-        width: width - 140,
-        marginTop: 17,
-    },
-    examDetail: {
+	container: {
+		backgroundColor: '#F6F6F6',
+		width: '100%',
+		height: '100%'
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+	titleContent: {
+		marginTop: Math.min(Math.max((height - 64 - 78 - (2 * 304)) / 3, 3), 10),
+		flexDirection: "row",
+		backgroundColor: "white",
+		height: 78,
+	},
+	arrow: {
+		position: 'absolute',
+		resizeMode: 'contain',
+		right: 18.8,
+		width: 7.4,
+	},
+	titleText: {
+		width: '70%',
+		height: '100%',
+		backgroundColor: "#fff",
+		marginLeft: 10,
+	},
+	h2: {
+		fontSize: 16,
+		lineHeight: 25,
+		color: "#172434",
+	},
+	examTitle: {
+		width: width - 235,
+		marginTop: 17,
+	},
+	choosePaperButton: {
+		position: 'absolute',
+		right: 15,
+		top:24,
+	},
+	examDetail: {
 
-    },
-    p: {
-        marginTop: 5,
-        fontSize: 12,
-        lineHeight: 20,
-        color: "#8E9091"
-    },
-    circleChart: {
-        position: 'absolute',
-        top: 10,
-        right: 40,
-        width: 60,
-        height: 60,
-    },
-    titleIcon: {
-        flex: 1,
-        backgroundColor: "#FFF",
-        justifyContent: 'center'
-    },
-    calendarView: {
-        position: 'relative',
-        backgroundColor: '#fff',
-        marginTop: Math.min(Math.max((height - 64 - 78 - (2 * 304)) / 3, 3), 10),
-    },
-    chartTitleContainer: {
-        flexDirection: "column",
-        backgroundColor: "#fff",
-        height: 78,
-        zIndex: 100
-    },
-    chartTopContainer: {
-        flexDirection: "row",
-        height: 48,
-    },
-    chartBottomContainer: {
-        flexDirection: "row",
-        width: "100%",
-        zIndex: 100,
-    },
-    h4: {
-        marginTop: 12,
-        marginLeft: 10,
-        fontSize: 16,
-        fontWeight: "400",
-        color: "#172434"
-    },
-    psmall: {
-        marginTop: 14,
-        marginLeft: 35,
-        fontSize: 12,
-        color: "#8E9091",
-        backgroundColor: 'rgba(0,0,0,0)'
-    },
-    average: {
-        position: 'absolute',
-        top: 14,
-        left: 175,
-        fontSize: 12,
-        color: '#8E9091',
-        backgroundColor: 'rgba(0,0,0,0)'
-
-    },
-    rightTitle: {
-        position: "absolute",
-        top: 12,
-        right: 50,
-        fontSize: 16
-    },
-    rightDetail: {
-        position: "absolute",
-        top: 14,
-        right: 49,
-        fontSize: 12,
-        color: "#8E9091",
-        backgroundColor: 'rgba(0,0,0,0)'
-    },
-    rightContainer: {
-        flexDirection: "row",
-        height: 68,
-        width: '100%',
-        position: "absolute",
-    },
-    greenBlock: {
-        marginLeft: 10,
-        marginTop: 31.5,
-        width: 15,
-        height: 15,
-    },
-    redBlock: {
-        marginLeft: 10,
-        marginTop: 14,
-        width: 15,
-        height: 15,
-    },
-    blueBlock: {
-        marginLeft: 10,
-        marginTop: 10,
-        width: 15,
-        height: 15,
-    },
-    separator: {
-        marginRight: 10,
-        marginLeft: 10,
-        height: 1,
-        backgroundColor: '#7A8FAC',
-        zIndex: 9,
-        opacity: 0.1,
-    }
+	},
+	p: {
+		marginTop: 5,
+		fontSize: 12,
+		lineHeight: 20,
+		color: "#8E9091"
+	},
+	circleChart: {
+		position: 'absolute',
+		top: 10,
+		right: 40,
+		width: 60,
+		height: 60,
+	},
+	titleIcon: {
+		flex: 1,
+		backgroundColor: "#FFF",
+		justifyContent: 'center'
+	},
+	calendarView: {
+		position: 'relative',
+		backgroundColor: '#fff',
+		marginTop: Math.min(Math.max((height - 64 - 78 - (2 * 304)) / 3, 3), 10),
+	},
+	chartTitleContainer: {
+		flexDirection: "column",
+		backgroundColor: "#fff",
+		height: 75,
+		zIndex: 100
+	},
+	chartTopContainer: {
+		flexDirection: "row",
+		height: 48,
+	},
+	chartBottomContainer: {
+		flexDirection: "row",
+		width: "100%",
+		zIndex: 100,
+	},
+	h4: {
+		marginTop: 12,
+		marginLeft: 10,
+		fontSize: 16,
+		fontWeight: "400",
+		color: "#172434"
+	},
+	psmall: {
+		marginTop: 14,
+		marginLeft: 35,
+		fontSize: 12,
+		color: "#8E9091",
+		backgroundColor: 'rgba(0,0,0,0)'
+	},
+	average: {
+		position: 'absolute',
+		top: 14,
+		left: 175,
+		fontSize: 12,
+		color: '#8E9091',
+		backgroundColor: 'rgba(0,0,0,0)'
+	},
+	rightTitle: {
+		position: "absolute",
+		top: 8,
+		right: 15,
+		fontSize: 13,
+	},
+	rightDetail: {
+		position: "absolute",
+		top: 14,
+		right: 49,
+		fontSize: 12,
+		color: "#8E9091",
+		backgroundColor: 'rgba(0,0,0,0)'
+	},
+	rightContainer: {
+		flexDirection: "row",
+		height: 68,
+		width: '100%',
+		position: "absolute",
+	},
+	button: {
+		paddingVertical: 7,
+		paddingHorizontal: 10,
+		borderRadius: 6,
+	},
+	greenBlock: {
+		marginLeft: 10,
+		marginTop: 31.5,
+		width: 15,
+		height: 15,
+	},
+	redBlock: {
+		marginLeft: 10,
+		marginTop: 14,
+		width: 15,
+		height: 15,
+	},
+	blueBlock: {
+		marginLeft: 10,
+		marginTop: 10,
+		width: 15,
+		height: 15,
+	},
+	separator: {
+		marginRight: 10,
+		marginLeft: 10,
+		height: 1,
+		backgroundColor: '#7A8FAC',
+		zIndex: 9,
+		opacity: 0.1,
+	}
 }
